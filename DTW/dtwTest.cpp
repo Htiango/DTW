e0a03952da4622c91f67cc2647d5923accb8accf
Htiango <hongty106@gmail.com>
2016-10-02 22:37:31 +0800

    5th commit. Test the segmental K-mean.

--- DTW/dtwTest.cpp
+++ DTW/dtwTest.cpp
@@ -19,7 +19,6 @@ string txtInputPath = "/Users/hty/desktop/testingData/input";
 //string txtInputPath = "/Users/hty/desktop/testingData/test1.0/input";
 
 void getTem(vector<vector<vector<double>>>& temGroup){
-    
     for (unsigned int i = 0; i < TYPE_NUM; i++) {
 //    for (unsigned int i = 0; i < 1; i++) {
         for(unsigned int j = 0; j < TEM_NUM; j ++){
@@ -37,7 +36,7 @@ void getTem(vector<vector<vector<double>>>& temGroup){
 
 
 void getInput(vector<vector<double>>& input, string& wavPath, string& txtPath){
-    capture(wavPath);
+//    capture(wavPath);
 //    getchar();
     featureExtraction(input, wavPath, txtPath);
 }
@@ -89,12 +88,67 @@ void getResult(){
 }
 
 
-//void getSegTem(){
-//    vector<vector<vector<double>>> temGroup;
-//    getTem(temGroup);
-//    
-//    vector<vector<double>> segTem;
-//    segTem = dtw2hmm(temGroup);
-//    cout << "yes!!"<<endl;
-//}
+void getSegTem(){
+    vector<vector<vector<double>>> segTemGroup;
+    for (int i = 0; i < TYPE_NUM; i++) {
+        vector<vector<vector<double>>> temGroup;
+        for (int j = 0; j < TEM_NUM; j++) {
+            cout << "-----------------------Template " << i << " Instance " << j <<"------------------------" << endl;
+            string wavpath = wavTemPath +  to_string(i) + "/" + to_string(j)+ "/record.wav";
+//            capture(wavpath);
+            vector<vector<double>> temFeature;
+            string txtpath = txtTemPath + to_string(i) + "/";
+            featureExtraction(temFeature, wavpath, txtpath);
+            temGroup.push_back(temFeature);
+        }
+        vector<vector<double>> segTem;
+        segTem = dtw2hmm(temGroup);
+        cout << "You have got the segment template!!!!!!!!!!!!!!!!!!!"<<endl;
+        segTemGroup.push_back(segTem);
+    }
+    
+    int correctNum =0;
+    
+    for (unsigned int i = 0; i < TYPE_NUM; i++) {
+        
+        for (unsigned int j = 0; j < INPUT_NUM; j++) {
+            cout << endl;
+            cout <<"-----------------------Input " << i <<"   Instance " << j << "------------------------" << endl;
+            vector<vector<double>> input;
+            string wavPath = wavInputPath + to_string(i) + "/" + to_string(j) + "/record.wav";
+            string txtPath = txtInputPath + to_string(i) + "/" + to_string(j) + "/";
+            getInput(input, wavPath, txtPath);
+            
+            double minDtw = dtw(input, segTemGroup[0]);
+            int bestIndex = 0;
+            cout << "The input is " << i << ", the dtwValue with 0.0 is " << minDtw << endl;
+            
+            for (unsigned int k = 1; k < TYPE_NUM; k++) {
+                double dtwValue = dtw(input, segTemGroup[k]);
+                cout << "The input is " << i << ", the dtwValue with "<< k  << " is " << dtwValue << endl;
+                if (dtwValue < minDtw) {
+                    bestIndex = k;
+                    minDtw = dtwValue;
+                }
+            }
+            cout << "The input is " << i << ", while the match template is " << bestIndex  << ". Their dtw value is "<< minDtw <<  endl;
+            
+            if (i == bestIndex) {
+                correctNum += 1;
+                cout << "The matching is correct!! Now the correct number is " << correctNum << endl;
+            }
+            else{
+                cout << "The matching is wrong......." << endl;
+            }
+        }
+    }
+    
+    cout << "The total number of input is "<< TYPE_NUM * INPUT_NUM << endl;
+    cout << "The correct matching input number is "<< correctNum << endl;
+    cout << "The accuracy of the matching is " << (1.0 * correctNum / (TYPE_NUM * INPUT_NUM)) << endl;
+    
+    
+}
+
+
 
